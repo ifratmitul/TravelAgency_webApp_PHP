af57d17911dfa39f88852ffdb3c22d5ed61bae68
ifrat Mitul <ifrat.mitul@hotmail.com>
2019-11-11 18:13:35 +0600

    Added modal in blog

--- blog.php
+++ blog.php
@@ -41,7 +41,7 @@
     </li>
     <li class="nav-item" >
 
-    <button class = "btn btn-info " > Create your own Blog</button>
+    <button class = "btn btn-info" data-toggle="modal" data-target="#myModal" > Create your own Blog</button>
 
     </li>
 
@@ -112,3 +112,61 @@
 
 
     <?php include ('footer.php'); ?>
+
+
+
+    <!-- Modal area-->
+
+
+                      <!-- Modal -->
+                      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Create your Blog</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                                <form id = "adPackage" method = "post">
+                                        <div class="form-group">
+                                          <label for="exampleFormControlInput1">Blog Title</label>
+                                          <input type="text" class="form-control" id="p_title" placeholder="Title">
+                                        </div>
+                                        <div class="form-group">
+                                          <label for="exampleFormControlSelect1">Location</label>
+                                          <select class="form-control" id="p_location" required>
+                                          <option >Select Location</option>
+                                            <option >Bangladesh</option>
+                                            <option>Hong Kong</option>
+                                            <option>USA</option>
+                                            <option>Uk</option>
+                                            <option>Germeny</option>
+                                          </select>
+                                        </div>
+
+                                        
+                                        <div class="form-group">
+                                          <label for="exampleFormControlTextarea1">Details</label>
+                                          <textarea class="form-control" id="p_details" rows="3"></textarea>
+                                        </div>
+
+                                        <div class="form-group">
+                                                <label for="exampleFormControlFile1">Add Location Picture</label>
+                                                <input type="file" class="form-control-file" id="exampleFormControlFile1" required>
+                                              </div>
+
+                                      </form>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="submit" form = "adPackage" onclick = "pack_validation()"class="btn btn-primary">Post</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  
+                  
+                  <!--modal-->
