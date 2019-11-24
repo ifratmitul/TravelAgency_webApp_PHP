6622b3cdaa33a6933863434ffbd4a053ffc9bfa3
Ifrat Mitul <ifrat.mitul@hotmail.com>
2019-11-24 17:19:39 +0600

    Eeverythin ok in here

--- action_page.php
+++ action_page.php
@@ -45,7 +45,7 @@ if(isset($_POST['u_regi'])){
         $password = md5($pass);
 
         //Neeed to change pic t varchar and at this moment Address is going into pic colm.
-        $sql = "INSERT INTO userList (Fname, Lname, pass, email, pic)
+        $sql = "INSERT INTO userList (Fname, Lname, pass, email, address)
         VALUES ('$fname', '$lname', '$password', '$email', '$address')";
         if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
@@ -61,44 +61,49 @@ if(isset($_POST['u_regi'])){
 
 }
    
-//login user : something is wrong here. Need to rebuild USERLIST DB again.
-    if(isset($_POST['login_user']))
+//login user : 
 
-    {
-        $errors = array(); 
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pass = mysqli_real_escape_string($conn, $_POST['pass1']);
+if(isset($_POST['login_user']))
+{
 
 
-        if(empty($email)){
-            array_push($errors, "Email is required");
-        }
-        if(empty($password)){
-            array_push($errors, "Password is required");
-        }
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, $_POST['pass1']);
 
-        if (count($errors ) == 0){
-            $password = md5($pass);
-            $query =  "SELECT * FROM userList WHERE email = '$email' AND pass = '$password'";
-            $result =  mysqli_query($conn, $query);
-            if(mysqli_num_rows($result)){
-                $_SESSION['email'] = $email;
-                $_SESSION['success'] = "You are now logged IN";
-                header('location: profile.php');
+
+
+    
+
+
+        $password = md5($pass); //neeed to change some DB col name
+        $query =  "SELECT * FROM userList WHERE email = '$email' AND pass = '$password'";
+        $result =  mysqli_query($conn, $query);
+        if(mysqli_num_rows($result)){
+            $_SESSION['email'] = $email;
+            $_SESSION['success'] = "You are now logged IN";
+
+            if (mysqli_num_rows($result) >0) 
+            {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) 
+            {
+            $_SESSION['fname'] = $row["Fname"];
+            //$_SESSION['lname'] = $row["Lname"];
+
             }
-            else{
-                array_push($errors, "Wrong Email or password. Please try again");
-                echo "Something is wrong";
             }
+
+
+            header('location: profile.php');
         }
-    }
+        else{
+            //array_push($errors, "Wrong Email or password. Please try again");
+            echo "Something is wrong";
+        }
+    
 
-    //adminlogin
 
-    if(isset($_POST['admin_login']))
-    {
-
-    }
+}
 
 
 
